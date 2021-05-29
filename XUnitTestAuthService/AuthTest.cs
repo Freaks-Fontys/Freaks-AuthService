81a928e722e00202cc3ee9682ea9c9d71d59099e
Twan Beeren <beerentwan2709@gmail.com>
2021-05-29 12:52:16 +0200

    first commit

---
+++ XUnitTestAuthService/AuthTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace XUnitTestAuthService
+{
+    public class AuthTest
+    {
+        [Fact]
+        public void Login()
+        {
+
+        }
+
+        [Fact]
+        public void Register()
+        {
+
+        }
+
+        [Fact]
+        public void GenerateJWT()
+        {
+
+        }
+    }
+}
