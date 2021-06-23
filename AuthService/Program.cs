6957f48a2ad35b46ba5d02ffb9dd36892b4dbf8c
Twan Beeren <beerentwan2709@gmail.com>
2021-06-23 16:07:26 +0200

    Removed logger

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -2,9 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog;
-using NLog.Extensions.Logging;
-using NLog.Web;
 using System;
 using System.IO;
 
@@ -14,65 +11,18 @@ namespace AuthService
     {
         public static void Main(string[] args)
         {
-            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true).Build();
-
-            LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
-            var logger = NLogBuilder.ConfigureNLog(LogManager.Configuration).GetCurrentClassLogger();
-
-            try
-            {
-                logger.Debug($"starting application '{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}'");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception exception)
-            {
-                logger.Error(exception, $"Stopped '{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}' because of an exception");
-                throw;
-            }
-            finally
-            {
-                LogManager.Shutdown();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) {
-            return Host
-                .CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    var env = hostingContext.HostingEnvironment;
-                    Console.WriteLine($"the environment is now: {env.EnvironmentName}");
-
-                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
-                                    optional: true, reloadOnChange: true);
-
-                    config.AddEnvironmentVariables("FREAKS_");
-                })
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder
-                    .UseStartup<Startup>()
-                    .ConfigureLogging(logging =>
-                    {
-                        logging.ClearProviders();
-                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                    });
-                }).UseNLog();
-
-            //Host.CreateDefaultBuilder(args)
-            //    .ConfigureWebHostDefaults(webBuilder =>
-            //    {
-            //        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            //        webBuilder.UseStartup<Startup>();
-            //        webBuilder.ConfigureAppConfiguration(config =>
-            //                            config.AddJsonFile($"appsettings.{env}.json"));
-            //    });
-        }
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(config =>
+                                        config.AddJsonFile($"appsettings.{env}.json"));
+                });
             
     }
 }
